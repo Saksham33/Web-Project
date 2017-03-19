726f25c8b18c8449b17df4db6eb155975fae037c
Saksham Narang <sakshamnarang@gmail.com>
2017-03-19 21:05:59 +0530

    Removing everything because front end is re-designed and having some merge conflicts. Will add evrything back shortly.

--- Project/static/create/controller/quesCtrl.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module("Ques",["myApp"]);
-
-app.controller("QuesCtrl", function($scope, shareUname) {
-	$scope.f = function() {
-		console.log(shareUname.uname);
-	}
-});
--- Project/static/create/js/controllers/quesCtrl.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("Ques",[]);
-
-app.controller("QuesCtrl", function($scope, $http) {
-	
-});
