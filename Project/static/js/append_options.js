0004bb70a11b628135e9c431b3db69feb9ab3584
Silversmack <silversmack@gmail.com>
2017-05-05 16:30:26 +0530

    Added page for creating coding questions

--- Project/static/js/append_options.js
+++ Project/static/js/append_options.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$("#b1").click(function() {
 		var v = $("#i1").val().trim();
-		if(v != "")
-			$("#options").append("<div data-toggle='buttons'><label class='btn active'><input type='radio' name='mcq' value='"+v+"' required><i class='fa fa-circle-o fa-2x'></i><i class='fa fa-dot-circle-o fa-2x'></i>&nbsp; &nbsp;<span>"+v+"</span></label>&nbsp; &nbsp;<button class='btn btn-danger btn-xs' style='margin-top: 1%;' onclick='del(this)'><div class='glyphicon glyphicon-remove'></div> Remove</button><br><br></div>");
+		var x = $("#i2").val().trim();
+		if(v != "" && x != "")
+			$("#options").append("<div data-toggle='buttons'><label class='btn active'><input type='radio' name='mcq' value='"+v+"' required><i class='fa fa-circle-o fa-2x'></i><i class='fa fa-dot-circle-o fa-2x'></i>&nbsp; &nbsp;<span>"+v+"</span></label><label class='btn active' style='margin-left: 5px';><input type='radio' name='mcq' value='"+x+"' required><i class='fa fa-circle-o fa-2x'></i><i class='fa fa-dot-circle-o fa-2x'></i>&nbsp; &nbsp;<span>"+x+"</span></label>&nbsp; &nbsp;<button class='btn btn-danger btn-xs' style='margin-top: 1%;' onclick='del(this)'><div class='glyphicon glyphicon-remove'></div> Remove</button><br><br></div>");
 		$("#i1").val("");
+		$("#i2").val("");
 	});
 });
 
